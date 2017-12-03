212b2ab125e6141c64ce018919b8f96a5efe0620
Brelon Buckner <brelonbuckner@Brelons-MacBook-Air.local>
2017-12-03 10:25:40 -0500

    Created two variables, used the Number method, and a if statement

--- guessingGame.js
+++ guessingGame.js
@@ -1,2 +1,9 @@
-var number = prompt("Guess a number!");
-console.log(number);
+// Create secret number
+var secretNumber = 3;
+// ask user for guess
+var guess = prompt("Guess a number");
+alert(guess);
+// check if guess is right
+if (Number(guess) === secretNumber) {
+	alert("You got it right!");
+}
