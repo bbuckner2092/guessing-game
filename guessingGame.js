c71c7f838b2d1b84efd93da1b684d33bc136a557
Brelon Buckner <brelonbuckner@Brelons-MacBook-Air.local>
2017-12-03 10:33:06 -0500

    Altered the if and else if statement to be more clear.  Placed Number method after the prompt

--- guessingGame.js
+++ guessingGame.js
@@ -1,9 +1,19 @@
 // Create secret number
 var secretNumber = 3;
 // ask user for guess
-var guess = prompt("Guess a number");
+var guess = Number(prompt("Guess a number"));
 alert(guess);
 // check if guess is right
-if (Number(guess) === secretNumber) {
+// use Number in the if statement
+if (guess === secretNumber) {
 	alert("You got it right!");
+}
+// Otherwise, check if higher
+else if (guess > secretNumber) {
+	alert("Too High.  Guess again");
+
+}
+// Otherwise, check if lower
+else {
+	alert("Too low.  Guess again!");
 }
